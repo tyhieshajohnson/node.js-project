7c55ccf3d91ca3a9decd555fe2e26913155416ae
tyhieshajohnson <johnsontyhiesha@gmail.com>
2024-02-21 15:39:43 +0200

    Home Page WIP

--- frontend/frontend-ty/src/views/HomeView.vue
+++ frontend/frontend-ty/src/views/HomeView.vue
@@ -1,14 +1,21 @@
 <template>
   <div class="background-image">
-    <Loader v-if="loading"/>
-    <div v-else>
-      <img
-      src="https://i.ibb.co/7vhH7MN/Gold-s-Gym.jpg"
-      alt="Magma-Sports-wear-Gym-Editorial-Shoot-2"
-      border="0"
-    /> 
-    </div> 
+    <Loader v-if="loading" />
+    <div class="home-content" v-else>
+      <img src="https://i.ibb.co/7vhH7MN/Gold-s-Gym.jpg" />
+      <div class="text-container">
+        <h1>SHOW YOUR <span style="color: #F9EF23;">BEST</span></h1>
+        <button>
+          <router-link
+              to="/services"
+              class="nav-link"
+              style="font-family: 'Manrope', sans-serif"
+              >OUR SERVICES
+            </router-link>
+        </button>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -26,13 +33,81 @@ export default {
   mounted() {
     setTimeout(() => {
       this.loading = false;
-    }, 5000);
+    }, 1000);
   },
 };
 </script>
 
+<!-- CSS -->
 <style scoped>
 .background-image img {
   width: 100%;
+}
+
+.home-content {
+  position: relative;
+}
+
+.text-container {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  color: white;
+}
+
+h1 {
+  font-size: 96px;
+  margin-bottom: 20px;
+  font-weight: 1000;
+}
+
+button {
+    padding: 10px 20px;
+    font-size: 1em;
+    background-color: #6D690B; 
+    color: white; 
+    border: none;
+    cursor: pointer;
+    font-weight: 400;
+    border-radius: 7px;
+}
+
+/* MEDIA QUERIES */
+@media only screen and (max-width: 600px) {
+  .background-image img {
+  width: 100%;
+}
+
+.home-content {
+  position: relative;
+}
+
+.text-container {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  color: white;
+}
+
+h1 {
+  font-size: larger;
+  margin-bottom: 20px;
+  font-weight: 1000;
+}
+
+button {
+    padding: 10px 20px;
+    font-size: 1em;
+    background-color: #6D690B; 
+    color: white; 
+    border: none;
+    cursor: pointer;
+    font-weight: 400;
+    border-radius: 7px;
+}
 }
 </style>
