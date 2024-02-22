6e6b813c1c4fe91c2590e7760890f5ec1e34ecfe
SIVUPE <sivuyisiwepeter79@gmail.com>
2024-02-22 08:51:58 +0200

    database

---
+++ backend/model/index.js
@@ -0,0 +1,9 @@
+import { Users } from "./Users.js";
+import { Products } from "./Products.js";
+// Create objects
+let users = new Users()
+let products = new Products()
+export{
+    users,
+    products
+}  
